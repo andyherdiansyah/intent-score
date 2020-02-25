e72e07b5a4669b440adc41dabd7a2ad5c1d03aa6
andyherdiansyah <aherdiansyah99@gmail.com>
2020-02-25 12:33:08 +0700

    Latihan Intent 1

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -3,12 +3,23 @@ package id.putraprima.skorbola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ResourceBundle;
 
 public class ResultActivity extends AppCompatActivity {
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        resultText = findViewById(R.id.textView3);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+
+            resultText.setText(bundle.getString("result"));
+        }
     }
 }
